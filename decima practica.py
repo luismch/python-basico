ea9a973ea9c2e1df59748f51044b09a93191e0fa
luismch <lh0202275202@gmail.com>
2022-05-24 15:16:27 -0500

    decima practica

---
+++ decima practica.py
@@ -0,0 +1,11 @@
+def run():
+    # nombre = input("escribe tu nombre: ")
+    # for letra in nombre:
+    #     print(letra)
+
+    frase = input("escribe una frase: ")
+    for caracter in frase:
+        print(caracter.upper())
+
+if __name__ == "__main__":
+    run()
