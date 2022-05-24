4e7c6100ca44ac745d9cd648c589187cd50d9af7
luismch <lh0202275202@gmail.com>
2022-05-24 16:11:56 -0500

    decimatercera practica

---
+++ decimatercera practica.py
@@ -0,0 +1,19 @@
+import random
+
+
+def run():
+    numero_aleatorio = random.randint(1, 100)
+    numero_elegido = int(input("elije un numero de 1 al 100: "))
+    while numero_elegido != numero_aleatorio:
+        if numero_elegido < numero_aleatorio:
+            print("busca un numero mas grande")
+        else:
+            print("busca un numero mas pequeño")
+        numero_elegido = int(input("elige otro numero: "))
+    print("¡GANASTE!")
+    
+
+
+
+if __name__ == "__main__":
+    run()
