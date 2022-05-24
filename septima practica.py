3af9942a0799773d140b63902ae30dd37ce9f7a3
luismch <lh0202275202@gmail.com>
2022-05-24 11:33:35 -0500

    septima practica

---
+++ septima practica.py
@@ -0,0 +1,14 @@
+contador = 0
+print("2 elevado a " + str(contador) + " es igual a: " + str(2**contador))
+
+contador = 1
+print("2 elevado a " + str(contador) + " es igual a: " + str(2**contador))
+
+contador = 2
+print("2 elevado a " + str(contador) + " es igual a: " + str(2**contador))
+
+contador = 3
+print("2 elevado a " + str(contador) + " es igual a: " + str(2**contador))
+
+contador = 4
+print("2 elevado a " + str(contador) + " es igual a: " + str(2**contador))
